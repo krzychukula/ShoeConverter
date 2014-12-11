33e1c0597e4de77c191a3269f68b5586d13bda12
krzychukula <krzychukula@gmail.com>
2014-12-11 16:09:16 +0100

    basic conversion

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -26,6 +26,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextField.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!
+        
+        let conversionConstant = 30
+        integerFromTextField += conversionConstant
+        
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        
         
     }
 
